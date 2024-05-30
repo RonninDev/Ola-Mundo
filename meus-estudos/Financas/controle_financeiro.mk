4bd14985df404a06ec5018b6d556cc4e5da5d8c2
RonninDev <ronnin.dev@gmail.com>
2024-05-30 09:24:01 -0300

    Atualizações de Diretórios e finalização do primeiro curso

---
+++ meus-estudos/Financas/controle_financeiro.mk
@@ -0,0 +1,14 @@
+Dívidas para Junho
+
+Nubank      798,24
+Itau        954,07 + 285,46 (disponível 164,54)
+Inter       877,65 (disponível 61,27)
+Aluguel     1200
+Condomínio
+Luz
+Telefone   
+Luciano    900
+Mãe        
+André      89
+
+3.618,96
