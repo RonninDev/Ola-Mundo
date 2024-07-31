772fc4ba209f5eb9a634a71280367f23fd0c2172
RonninDev <ronnin.dev@gmail.com>
2024-07-30 23:10:53 -0300

    30-07-24

--- meus-estudos/algoritmos-curso-em-video/primeiro.py
+++ meus-estudos/algoritmos-curso-em-video/primeiro.py
@@ -4,3 +4,19 @@ print("Me livrei da maldição")
 print("O seu primeiro algoritmo é me dar seu nome.")
 nome = input("Digite seu Nome:")
 print("Olá, " + nome + "!")
+
+
+
+print("Crie um Algoritmo que leia os lados de um triângulo e diga se ele é Equilátero ou Escaleno")
+
+l1 = int(input('Digite o primeiro lado: '))
+l2 = int(input('Digite o segundo lado: '))
+l3 = int(input('Digite o terceiro lado: '))
+
+tringulo = (l1 < l2 + l3) and (l2 < l1 + l3) and (l3 < l1 + l2)
+equilatero = (l1 == l2) and (l2 == l3)
+escaleno = (l1 != l2) and (l2 != l3) and (l1 != l3)
+
+print(equilatero)
+print(escaleno)
+
